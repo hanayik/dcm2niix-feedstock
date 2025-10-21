cac979df3e770011b8ace1074eb4b580ca5221da
Taylor Hanayik <hanayik@gmail.com>
2025-10-21 16:59:49 +0100

    update build args to unify jpeg use

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..\
+cmake -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 cmake --build . --target INSTALL --config Release
