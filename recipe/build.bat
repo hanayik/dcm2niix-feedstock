4e5c65186a2f861bd19b0fa7f939dd0eba8f4f0c
Taylor Hanayik <hanayik@gmail.com>
2025-10-21 18:05:30 +0100

    maybe this works

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,5 +1,5 @@
 tar -xvf ..\dcm2niix-*.tar.gz --strip-components=1
-mkdir build
-cd build
-cmake -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+mdkir %SRC_DIR%\build
+cd %SRC_DIR%\build
+cmake -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
 cmake --build . --target INSTALL --config Release
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 ls -lh .
-# untar dcm2niix-*.tar.gz
-tar -xvf ../dcm2niix-*.tar.gz --strip-components=1
-ls -lh .
-mkdir build && cd build
-cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
+mkdir ${SRC_DIR}/build
+cd ${SRC_DIR}/build
+cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ${SRC_DIR}
 make install
