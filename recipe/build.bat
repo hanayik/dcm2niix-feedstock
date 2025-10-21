05a04310f3d1f51837e1e269e74413cc8bbc01d5
Taylor Hanayik <hanayik@gmail.com>
2025-10-21 17:53:49 +0100

    untar the sources

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,3 +1,4 @@
+tar -xvf ..\dcm2niix-*.tar.gz --strip-components=1
 mkdir build
 cd build
 cmake -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
+ls -lh .
+# untar dcm2niix-*.tar.gz
+tar -xvf ../dcm2niix-*.tar.gz --strip-components=1
 ls -lh .
 mkdir build && cd build
 cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
