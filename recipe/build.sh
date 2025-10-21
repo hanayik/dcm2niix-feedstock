318550ba9149794203da25b1bedf075803d6cd3e
Taylor Hanayik <hanayik@gmail.com>
2025-10-21 17:39:34 +0100

    list contents

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+ls -lh .
 mkdir build && cd build
 cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
 make install
