eed5bbc71e9b46f5c429a21bc391e7232e14c490
Taylor Hanayik <hanayik@gmail.com>
2025-10-23 11:48:32 +0100

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ tar -xzf dcm2niix-*.tar.gz
 ls -lh .
 cd dcm2niix-*/
 mkdir build && cd build
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
+cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
 make install
 # copy license.txt from $SRC_DIR/dcm2niix-*/license.txt to $SRC_DIR/license.txt
 cd ..
