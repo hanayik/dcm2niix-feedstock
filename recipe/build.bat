4e75c20bce6cbecb690e5d5097d9c6c57be2a348
Taylor Hanayik <hanayik@gmail.com>
2025-10-23 10:46:29 +0100

    cd to parent dir for license file discovery

--- recipe/build.bat
+++ recipe/build.bat
@@ -7,3 +7,4 @@ mkdir build
 cd build
 cmake -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 cmake --build . --target INSTALL --config Release
+cd ..
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,3 +6,4 @@ cd dcm2niix-*/
 mkdir build && cd build
 cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -Wno-dev -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
 make install
+cd ..
